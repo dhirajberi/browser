cbf0871dff768d9da2a53486d8df70c5b1a8df6b
Dhiraj Beri <46566839+dhirajberi@users.noreply.github.com>
2021-10-28 14:56:02 +0530

    Tabs feature added, permissions

---
+++ camera.py
@@ -0,0 +1,25 @@
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWebEngineWidgets import QWebEngineView, QWebEnginePage
+from PyQt5.QtCore import QUrl
+
+class WebEnginePage(QWebEnginePage):
+    def __init__(self, *args, **kwargs):
+        QWebEnginePage.__init__(self, *args, **kwargs)
+        self.featurePermissionRequested.connect(self.onFeaturePermissionRequested)
+
+    def onFeaturePermissionRequested(self, url, feature):
+        if feature in (QWebEnginePage.MediaAudioCapture, 
+            QWebEnginePage.MediaVideoCapture, 
+            QWebEnginePage.MediaAudioVideoCapture):
+            self.setFeaturePermission(url, feature, QWebEnginePage.PermissionGrantedByUser)
+        else:
+            self.setFeaturePermission(url, feature, QWebEnginePage.PermissionDeniedByUser)
+
+app = QApplication([])
+
+view = QWebEngineView()
+page = WebEnginePage()
+view.setPage(page)
+view.load(QUrl("https://meet.new"))
+view.show()
+app.exec_()
